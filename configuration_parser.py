d45d46009b446ba53cc4f3c995c6dad3c0dba71c
talrefter <talrefter0804@gmail.com>
2023-06-26 13:45:20 +0300

    add parsing for configuration file

---
+++ configuration_parser.py
@@ -0,0 +1,17 @@
+import sys
+import json
+
+NUM_ARGS = 2
+FILENAME_INDEX = 1
+
+def parse():
+    if len(sys.argv) != NUM_ARGS:
+        print("invalid argument number")
+        exit()
+    
+    try:
+        with open(sys.argv[FILENAME_INDEX]) as conf_file:
+            conf_data = json.load(conf_file)
+
+if __name__ == "__main__":
+    parse()
